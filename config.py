09c4da19f0bba61413779ee223ff19bab1a7709a
Deepti Dhanwate <dhanwatedeepti2@gmail.com>
2026-07-05 13:47:55 +0530

    Fix SQLite path for Render

--- config.py
+++ config.py
@@ -8,8 +8,8 @@ class Config:
     "super-secret-key-change-this"
     )
 
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'instance', 'database.db')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'database.db')    
+SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     MAX_REQUESTS_PER_SECOND = 10
     BRUTE_FORCE_LIMIT = 5
