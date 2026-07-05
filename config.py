b5706171f3234a604293e974b104de7c1573ff88
Deepti Dhanwate <dhanwatedeepti2@gmail.com>
2026-07-05 14:25:05 +0530

    Fix config.py indentation

--- config.py
+++ config.py
@@ -4,12 +4,12 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = os.environ.get(
-    "SECRET_KEY",
-    "super-secret-key-change-this"
+        "SECRET_KEY",
+        "super-secret-key-change-this"
     )
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'database.db')    
-SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'database.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     MAX_REQUESTS_PER_SECOND = 10
     BRUTE_FORCE_LIMIT = 5
